8aea75d1adccf7695577daba9392eedd910f7f4f
Jesse Collis <jesse@jcmultimedia.com.au>
2011-11-02 15:21:15 +1100

    updated runner main.m

--- JCPriorityQueue/main.m
+++ JCPriorityQueue/main.m
@@ -13,50 +13,37 @@ int main (int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
-      PriorityObject *ob1 = [[PriorityObject alloc] initWithValue:50];
-      PriorityObject *ob2 = [[PriorityObject alloc] initWithValue:40];
-      PriorityObject *ob3 = [[PriorityObject alloc] initWithValue:30];
-      PriorityObject *ob4 = [[PriorityObject alloc] initWithValue:60];
-      PriorityObject *ob5 = [[PriorityObject alloc] initWithValue:80];
-      PriorityObject *ob6 = [[PriorityObject alloc] initWithValue:10];
 
       JCPriorityQueue *queue = [[JCPriorityQueue alloc] init];
+      for (int i = 0; i< 300; i++)
+      {
+        [queue addObject:[[PriorityObject alloc] initWithValue:50]];
+        [queue addObject:[[PriorityObject alloc] initWithValue:40]];
+        [queue addObject:[[PriorityObject alloc] initWithValue:60]];
+        [queue addObject:[[PriorityObject alloc] initWithValue:30]];
+        [queue addObject:[[PriorityObject alloc] initWithValue:80]];
+        [queue addObject:[[PriorityObject alloc] initWithValue:10]];
+        [queue addObject:[[PriorityObject alloc] initWithValue:5]];
+        [queue addObject:[[PriorityObject alloc] initWithValue:5]];
+        [queue addObject:[[PriorityObject alloc] initWithValue:1]];
+      }
+
+      for (int i = 0; i < 100; i++)
+      {
+        NSLog(@"%@",[queue popFirst]);
+      }
       
-//      NSMutableArray *m_array = [NSMutableArray array];
-//      [m_array insertObject:ob1 atIndex:0];
-//      [m_array insertObject:ob2 atIndex:3];
-      
-      
-//      NSLog(@"%@", m_array);
-      [queue addObject:ob1];
-      //NSLog(@"Queue first: %lu Queue Count: %lu", [[queue first] value], [queue count]);
-      //NSLog(@"Queue: %@", queue);
-      
-      [queue addObject:ob2];
-      //NSLog(@"Queue first: %lu Queue Count: %lu", [[queue first] value], [queue count]);
-      //NSLog(@"Queue: %@", queue);
-      
-      [queue addObject:ob3];
-      ///NSLog(@"Queue first: %lu Queue Count: %lu", [[queue first] value], [queue count]);
-      //NSLog(@"Queue: %@", queue);
-      
-      [queue addObject:ob4];
-    //  NSLog(@"Queue first: %lu Queue Count: %lu", [[queue first] value], [queue count]);
-      //NSLog(@"Queue: %@", queue);
-      
-      [queue addObject:ob5];
-  //    NSLog(@"Queue first: %lu Queue Count: %lu", [[queue first] value], [queue count]);
-      //NSLog(@"Queue: %@", queue);
-      
-      [queue addObject:ob6];
-      
-      [queue addObject:[[PriorityObject alloc] initWithValue:10]];
-      [queue addObject:[[PriorityObject alloc] initWithValue:5]];
-      [queue addObject:[[PriorityObject alloc] initWithValue:5]];
-      [queue addObject:[[PriorityObject alloc] initWithValue:1]];
-//      NSLog(@"Queue first: %lu Queue Count: %lu", [[queue first] value], [queue count]);
-      NSLog(@"Queue: %@", queue);
+//    NSLog(@"Queue first: %lu Queue Count: %lu", [[queue first] value], [queue count]);
+//      NSLog(@"Queue: %@", queue);
+//
+//      [queue popFirst];
+//      NSLog(@"Queue: %@", queue);
+//      
+//      [queue popFirst];
+//      NSLog(@"Queue: %@", queue);
+//      
+//      [queue popFirst];
+//      NSLog(@"Queue: %@", queue);      
       
     }
     return 0;
