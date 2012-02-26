aa6a88e3465f500651e26ac8ad7a1c591f8b95d3
Jesse Collis <jesse@jcmultimedia.com.au>
2012-02-26 23:22:10 +1100

    Added more tests for larger number of inserts.

--- Unit Tests/JCPriorityQueueTests.m
+++ Unit Tests/JCPriorityQueueTests.m
@@ -12,10 +12,17 @@
 
 @interface JCPriorityQueueTests (){
   JCPriorityQueue *_queue;
-  NSInteger *_details;
+  NSInteger _details[25];
 }
 
 @end
+
+static const  NSInteger testData[25] = {
+  10,20,40,90,100,
+  320,900,657,225,854,
+  1689,5465,5123,6548,0,
+  63876,32656,12368,95641,5,
+  -123,-12345,-123456,-987,-43};
 
 @implementation JCPriorityQueueTests
 
@@ -23,14 +30,8 @@
 {
   [super setUp];
   _queue = [[JCPriorityQueue alloc] init];
-  
-  NSInteger some_data[20] = {
-    10,20,40,90,100,
-    320,900,657,225,854,
-    1689,5465,5123,6548,0,
-    63876,32656,12368,95641,5 };
-  
-  _details = some_data;
+
+  memcpy(_details, testData, sizeof(testData));
 }
 
 - (void)tearDown
@@ -46,6 +47,17 @@
   STAssertNil(first,@"Empty Queue should return nil if empty");
 }
 
+- (void)testPriorityQueueRemovesAnItemWhenItSaysItDoes
+{
+  [_queue addObject:[PriotityTestObject objectWithValue:10]];
+  [_queue addObject:[PriotityTestObject objectWithValue:20]];
+  
+  [_queue popFirst];
+  
+  STAssertEquals([_queue count], (NSUInteger)2, @"add two, pop one should leave a count of two (default is count of 1)");
+}
+
+
 - (void)testPriorityQueueReturnsCorrectObjectWhenZeroAddedAsSingleObject
 {
   id first_object = [PriotityTestObject objectWithValue:0]; 
@@ -56,7 +68,7 @@
   STAssertEquals(first_object, first_returned, @"Adding one object then popping that object should return the same objet");
 }
 
-- (void)testPriorityQueueManagesNegativeValues
+- (void)testPriorityQueueHandlesANegativeValue
 {
   id first_object = [PriotityTestObject objectWithValue:-49876];
   [_queue addObject:first_object];
@@ -66,7 +78,7 @@
   STAssertEquals(first_object, first_returned, @"Adding one object then popping that object should return the same objet");
 }
 
-- (void)testPriorityQueueHandlesSingleObjectNotZero
+- (void)testPriorityQueueHandlesSinglePositiveObject
 {
   id<JCPriorityQueueObject> first_object = [PriotityTestObject objectWithValue:10];
   [_queue addObject:first_object];
@@ -75,6 +87,54 @@
   
   STAssertEquals(first_object, first_returned, @"First element returned should be the same object originally added");
   STAssertEquals(first_object.value, (NSInteger)10, @"First element should be equal to the element added. Value was %d", first_returned.value);
+}
+
+- (void)testPriorityQueueHandlesAFewPredictableValues
+{
+  [_queue addObject:[PriotityTestObject objectWithValue:20]];
+  [_queue addObject:[PriotityTestObject objectWithValue:10]];
+  [_queue addObject:[PriotityTestObject objectWithValue:30]];
+  [_queue addObject:[PriotityTestObject objectWithValue:40]];
+  
+  id <JCPriorityQueueObject> obj = [_queue popFirst];
+  STAssertTrue(obj.value == 10, nil);
+  
+  obj = [_queue popFirst];
+  STAssertTrue(obj.value == 20, nil);
+
+  obj = [_queue popFirst];
+  STAssertTrue(obj.value == 30, nil);
+
+  obj = [_queue popFirst];
+  STAssertTrue(obj.value == 40, nil);
+  
+  obj = [_queue popFirst];
+  STAssertTrue(obj == nil, nil);
+}
+
+- (void)testPriorityQueueHandlesLotsOfRandomValues
+{
+  NSInteger lowest_value = NSIntegerMax;
+  size_t size_of_details = sizeof(_details) / sizeof(*_details);
+
+  srand(time(NULL));
+
+  for (int i = 0; i < 500; i++)
+  {
+    int index = rand() % size_of_details;
+    NSInteger value = _details[index];
+
+    if (value < lowest_value)
+    {
+      lowest_value = value;
+    }
+
+    [_queue addObject:[PriotityTestObject objectWithValue:value]];
+  }
+  
+  id<JCPriorityQueueObject> first_out = [_queue popFirst];
+  
+  STAssertEquals(first_out.value, lowest_value, @"Popping the first value should be equal to the lowest entered");
 }
 
 @end
